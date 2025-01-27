b8fc2637a1a30b5d232fb2da950d6c247fa181e1
Knerdly <plant-trialed.0v@icloud.com>
2025-01-27 11:52:36 -0500

    Revert "[DOT-154] Compiles"
    This reverts commit cd6ac308e8b7ce16e399b5b0f414c12ff53987a3.

--- keyboards/keychron/q1_max/ansi_encoder/keymaps/eponym/rules.mk
+++ keyboards/keychron/q1_max/ansi_encoder/keymaps/eponym/rules.mk
@@ -3,15 +3,6 @@ LEADER_TIMEOUT = 300
 
 TAP_DANCE_ENABLE = yes
 
-# Custom source file based on toggles:
-ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
-	SRC += ./tap_dance/tap_dance.c
-endif
-
-ifeq ($(strip $(LEADER_KEY_ENABLE)), yes)
-	SRC += ./leader_key/leader_key.c
-endif
-
-ifeq ($(strip $(RGB_ENABLE)), yes)
-	SRC += ./rgb/rgb.c
-endif
+# Custom source files:
+SRC += ./features/tap_dance/tap_dance.c
+SRC += ./features/leader_key/leader_key.c
