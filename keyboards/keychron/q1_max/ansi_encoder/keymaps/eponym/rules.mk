e7095f8c84df7e4750232217db069fe92a33605b
Knerdly <plant-trialed.0v@icloud.com>
2025-01-27 11:52:17 -0500

    [DOT-154] Trying to rollback

--- keyboards/keychron/q1_max/ansi_encoder/keymaps/eponym/rules.mk
+++ keyboards/keychron/q1_max/ansi_encoder/keymaps/eponym/rules.mk
@@ -2,16 +2,16 @@ LEADER_ENABLE = yes
 LEADER_TIMEOUT = 300
 
 TAP_DANCE_ENABLE = yes
+# SRC += tap_dance/tap_dance.c
+# # # Custom source file based on toggles:
+# # ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
+# # 	SRC += ./tap_dance/tap_dance.c
+# # endif
 
-# Custom source file based on toggles:
-ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
-	SRC += ./tap_dance/tap_dance.c
-endif
+# ifeq ($(strip $(LEADER_KEY_ENABLE)), yes)
+# 	SRC += ./leader_key/leader_key.c
+# endif
 
-ifeq ($(strip $(LEADER_KEY_ENABLE)), yes)
-	SRC += ./leader_key/leader_key.c
-endif
-
-ifeq ($(strip $(RGB_ENABLE)), yes)
-	SRC += ./rgb/rgb.c
-endif
+# ifeq ($(strip $(RGB_ENABLE)), yes)
+# 	SRC += ./rgb/rgb.c
+# endif
